4661956277b3474877216d97cfc1718f9c14dd39
Ruben Quintero <Ruben@Rubens-MacBook-Pro.local>
2013-05-28 22:13:45 -0500

    Initial Commit

---
+++ StoppingPower/BSPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSPAppDelegate.h
+//  StoppingPower
+//
+//  Created by Ruben Quintero on 5/28/13.
+//  Copyright (c) 2013 Ruben Quintero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoppingPower/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoppingPower
+//
+//  Created by Ruben Quintero on 5/28/13.
+//  Copyright (c) 2013 Ruben Quintero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BSPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BSPAppDelegate class]));
+    }
+}
